5105572d5ab06cad03ba165c3c6b6514efb1a46d
Doğukan İSPİRLİ <51451598+f3nr1rs3c@users.noreply.github.com>
2024-08-23 04:30:24 +0300

    Update Master_Scanner.py

--- Master_Scanner.py
+++ Master_Scanner.py
@@ -23,7 +23,7 @@ def print_menu():
 {0}4){2} Operating System Scanning
 {0}5){2} Firewall Scanning
 {0}6){2} TCP and UDP Scanning
-{0}7){2} ARP Scanning
+{0}7){2} ARP Scanning - (192.168.1.0/24)
 {0}8){2} Vulnerability Scanning - SQL Injection
 {0}9){2} Vulnerability Scanning - XSS Injection
 {0}10){2} Firewall Bypass
@@ -31,59 +31,67 @@ def print_menu():
 {0}12){2} Anonymous FTP Login Scanning
 {0}13){2} Mysql Scanning
 {0}14){2} VMware Version Scanning
-{0}15){2} Fake Mac Scanning
+{0}15){2} Fake Mac Scanning - (192.168.1.0/24)
 {0}16){2} Filtered Port Scanning
+{0}17){2} Detailed Scanning
+{0}18){2} Local Devices Scanning - (192.168.1.0/24)
 {0}X) Exit Program
 
-    """.format(Fore.RED, "0", Fore.RESET, "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", Fore.RESET))
+    """.format(Fore.RED, "0", Fore.RESET))
 
-def speed_scanning(target_ip):
-    os.system("nmap " + target_ip)
+def speed_scanning(target_ip, report_file): # Sistem üzerinde hızlı tarama yapmaya olanak sağlıyor.
+    os.system(f"nmap -oN {report_file} " + target_ip)
 
-def multi_scanning(target_ip):
-    os.system("nmap -D RND:10 " + target_ip)
+def multi_scanning(target_ip, report_file): # Sistem üzerinde çoklu IP kullanarak anonim bir tarama gerçekleştiriyor
+    os.system(f"nmap -oN {report_file} -D RND:10 " + target_ip)
 
-def service_version_scanning(target_ip):
-    os.system("nmap -sS -sV " + target_ip)
+def service_version_scanning(target_ip, report_file): # Sistem'in servisleri ve versiyonları hakkında bilgi almak için gerçekleşen bir taramadır.
+    os.system(f"nmap -oN {report_file} -sS -sV " + target_ip)
 
-def operating_system_scanning(target_ip):
-    os.system("nmap -sS -O " + target_ip)
+def operating_system_scanning(target_ip, report_file): # Sistem'in işletim sistemi hakkında bilgi almak için kullanılan bir taramadır.
+    os.system(f"nmap -oN {report_file} -sS -O " + target_ip)
 
-def firewall_scanning(target_ip):
-    os.system("nmap -v -f -sA " + target_ip)
+def firewall_scanning(target_ip, report_file): # Sistem içerisinde ki güvenlik duvarı hakkında bilgi almak için kullanılan bir tarama çeşididir.
+    os.system(f"nmap -oN {report_file} -v -f -sA " + target_ip)
 
-def tcp_udp_scanning(target_ip):
-    os.system("nmap -p T:20-25,80,443 U:53 " + target_ip)
+def tcp_udp_scanning(target_ip, report_file): # TCP paketleri ve UDP paketleri göndererek gerçekleşen taramadır.
+    os.system(f"nmap -oN {report_file} -p T:20-25,80,443 U:53 " + target_ip)
 
-def arp_scanning(target_it):
-    os.system("nmap -PR " + target_it)
+def arp_scanning(target_ip, report_file): # Arp paketleri göndererek yerel ağdaki cihazları tanımlamak için kullanılan bir tarama çeşididir.
+    os.system(f"nmap -oN {report_file} -PR " + target_ip)
 
-def sql_injection_scanning(target_ip):
-    os.system("nmap -p80 --script http-sql-injection " + target_ip)
+def sql_injection_scanning(target_ip, report_file): # Sistemde ki SQL Injection zafiyetleri tespit etmek için kullanılan bir tarama çeşididir.
+    os.system(f"nmap -oN {report_file} -p80 --script http-sql-injection " + target_ip)
 
-def xss_scanning(target_ip):
-    os.system("nmap -p80 --script http-dombased-xss.nse " + target_ip)
+def xss_scanning(target_ip, report_file): # Sistemde ki XSS zafiyetleri tespit etmek için kullanılan bir tarama çeşididir.
+    os.system(f"nmap -oN {report_file} -p80 --script http-dombased-xss.nse " + target_ip)
 
-def firewall_bypass(target_ip):
-    os.system("nmap -sF -p1-100 -T4 " + target_ip)
+def firewall_bypass(target_ip, report_file): # Sistemin güvenlik duvarını atlatarak tarama yapmaya olanak tanır.
+    os.system(f"nmap -oN {report_file} -sF -p1-100 -T4 " + target_ip)
 
-def script_scanning(target_ip):
-    os.system("nmap -sC " + target_ip)
+def script_scanning(target_ip, report_file): # Zafiyetler hakkında bilgi almakta büyük rol oynayan bir tarama çeşididir.
+    os.system(f"nmap -oN {report_file} -sC " + target_ip)
 
-def anonymous_ftp_scanning(target_ip):
-    os.system("nmap -p 21 --script=ftp-anon -PN -n ")
+def anonymous_ftp_scanning(target_ip, report_file): # Gizli FTP servis taraması gerçekleştiren bir tarama çeşididir.
+    os.system(f"nmap -oN {report_file} -p 21 --script=ftp-anon -PN -n ")
 
-def mysql_scanning(target_ip):
-    os.system("nmap -p 445 --script ms-sql-info")
+def mysql_scanning(target_ip, report_file): # Mysql veritabanı hakkında bilgi almak için gerçekleştirilen taramadır.
+    os.system(f"nmap -oN {report_file} -p 445 --script ms-sql-info")
 
-def vmware_scanning(target_ip):
-    os.system("nmap --script vmware-version -p443")
+def vmware_scanning(target_ip, report_file): # Sistem içerisinde sanallaştırm VMware teknolojisi taramasıdır.
+    os.system(f"nmap -oN {report_file} --script vmware-version -p443")
 
-def fake_mac_scanning(target_ip):
-    os.system("nmap -V -sT -PN --spoof-mac 0")
+def fake_mac_scanning(target_ip, report_file): # Sağda mac adresleri ile sistemdeki cihazları taramak için gerçekleştiren tarama çeşididir.
+    os.system(f"nmap -oN {report_file} -V -sT -PN --spoof-mac 0")
 
-def filtred_scanning(target_ip):
-    os.system("nmap -Pn " + target_ip)
+def filtred_scanning(target_ip, report_file): # Sistemdeki filtreli servisleri belirlemek için gerçekleşen taramadır.
+    os.system(f"nmap -oN {report_file} -Pn " + target_ip)
+
+def detailed_scanning(target_ip, report_file): # Sistem hakkında daha detaylı bilgi almak için gerçekleştirilen bir tarama çeşididir.
+    os.system(f"nmap -oN {report_file} -A " + target_ip)
+
+def local_devices_scanning(target_ip, report_file): # Yerel ağdaki cihazları tanımlamak için gerçekleştirilen taramadır.
+    os.system(f"nmap -oN {report_file} -sn " + target_ip)
 
 def main():
     clear_screen()
@@ -97,57 +105,66 @@ def main():
             print(Fore.RED + "Exiting The Program..." + Fore.RESET)
             break
       
-      # Hedef IP Girintisi  
+        # Hedef IP Girintisi  
         target_ip = input(Fore.GREEN + "Enter Target IP: " + Fore.RESET)
         
+        # Dosya adı ve uzantısı soruluyor
+        report_file = input(Fore.YELLOW + "Enter the report file name with extension (example: report.txt): " + Fore.RESET)
+        
         if choice == "1":
-            speed_scanning(target_ip)
+            speed_scanning(target_ip, report_file)
         
         elif choice == "2":
-            multi_scanning(target_ip)
+            multi_scanning(target_ip, report_file)
             
         elif choice == "3":
-            service_version_scanning(target_ip)
+            service_version_scanning(target_ip, report_file)
         
         elif choice == "4":
-            operating_system_scanning(target_ip)
+            operating_system_scanning(target_ip, report_file)
         
         elif choice == "5":
-            firewall_scanning(target_ip)
+            firewall_scanning(target_ip, report_file)
         
         elif choice == "6":
-            tcp_udp_scanning(target_ip)
+            tcp_udp_scanning(target_ip, report_file)
         
         elif choice == "7":
-            arp_scanning(target_ip)
+            arp_scanning(target_ip, report_file)
         
         elif choice == "8":
-            sql_injection_scanning(target_ip)
+            sql_injection_scanning(target_ip, report_file)
         
         elif choice == "9":
-            xss_scanning(target_ip)
+            xss_scanning(target_ip, report_file)
         
         elif choice == "10":
-            firewall_bypass(target_ip)
+            firewall_bypass(target_ip, report_file)
         
         elif choice == "11":
-            script_scanning(target_ip)
+            script_scanning(target_ip, report_file)
         
         elif choice == "12":
-            anonymous_ftp_scanning(target_ip)
+            anonymous_ftp_scanning(target_ip, report_file)
         
         elif choice == "13":
-            mysql_scanning(target_ip)
+            mysql_scanning(target_ip, report_file)
         
         elif choice == "14":
-            vmware_scanning(target_ip)
+            vmware_scanning(target_ip, report_file)
         
         elif choice == "15":
-            fake_mac_scanning(target_ip)
+            fake_mac_scanning(target_ip, report_file)
         
         elif choice == "16":
-            filtred_scanning(target_ip)
+            filtred_scanning(target_ip, report_file)
         
+        elif choice == "17":
+            detailed_scanning(target_ip, report_file)
+        
+        elif choice == "18":
+            local_devices_scanning(target_ip, report_file)
+                           
         else:
             print("Invalid input! Please try again.")
 
